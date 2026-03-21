e3e07844a3eebd506b5f42d7a3222d349f0b9277
Diver202 <divyanshsharma141206@gmail.com>
2026-03-21 18:11:42 +0530

    If god exists this will work

--- src/mongo_engine.py
+++ src/mongo_engine.py
@@ -15,7 +15,6 @@ def determineMongoStrategy(fieldMetadata):
         isNested = field.get("is_nested", False)
         isArray = field.get("is_array", False)
 
-
         if isArray or isNested:
             mongoStrategyMap[fieldName] = "reference"
         else:
@@ -23,22 +22,40 @@ def determineMongoStrategy(fieldMetadata):
             
     return mongoStrategyMap
 
+def processNode(dataNode, currentPath, dbInstance, strategyMap):
+    if isinstance(dataNode, dict):
+        processedDict = {}
+        for key, value in dataNode.items():
+            fieldPath = f"{currentPath}.{key}" if currentPath else key
+            strategy = strategyMap.get(fieldPath, "embed")
+            
+            processedValue = processNode(value, fieldPath, dbInstance, strategyMap)
+            
+            if strategy == "reference":
+                refCollection = dbInstance[fieldPath]
+                insertResult = refCollection.insert_one({"data": processedValue})
+                processedDict[key] = insertResult.inserted_id
+            else:
+                processedDict[key] = processedValue
+        return processedDict
+        
+    elif isinstance(dataNode, list):
+        processedList = []
+        for item in dataNode:
+            elementPath = f"{currentPath}[]"
+            processedItem = processNode(item, elementPath, dbInstance, strategyMap)
+            processedList.append(processedItem)
+        return processedList
+        
+    else:
+        return dataNode
+
 def processMongoData(mongoData, strategyMap, dbInstance):
     mainCollection = dbInstance["main_records"]
     
     for record in mongoData:
-        mainDocument = {}
-        for key, value in record.items():
-            strategy = strategyMap.get(key, "embed")
-            
-            if strategy == "reference":
-                refCollection = dbInstance[key]
-                insertResult = refCollection.insert_one({"data": value})
-                mainDocument[key] = insertResult.inserted_id
-            else:
-                mainDocument[key] = value
-                
-        mainCollection.insert_one(mainDocument)
+        processedRecord = processNode(record, "", dbInstance, strategyMap)
+        mainCollection.insert_one(processedRecord)
 
 def runMongoEngine():
     mongoUri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
